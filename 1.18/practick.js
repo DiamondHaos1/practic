2490ae3a2c3fe1cb2d6404423f3bf79a6b26b878
Bohdan Hladkiy <wawdwaddw@gmail.com>
2025-05-15 10:32:26 +0300

    complere array

---
+++ 1.18/practick.js
@@ -0,0 +1,13 @@
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    return console.log(`Семья состоит из: ${family} `)
+}
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+  return console.log(family.toString().toLowerCase())
+}
+
+standardizeStrings(favoriteCities)
